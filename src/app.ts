0a0d1077e1035f4abca391e7eb2c38cb4c572144
Galla <gallant.thomas@outlook.com>
2023-04-09 12:25:34 -0400

    Changing public endpoint response

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(cors({
 const baseRoute: string = "/api/v1";
 
 app.get(baseRoute, function (req, res) {
-    res.send("UserActiveGamesBFF");
+    res.send("UserGameStatisticsBFF");
 });
 
 const globalRouter = require('./app/routes/routes');
